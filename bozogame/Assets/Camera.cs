df419aae154a03355b0b5c9b7c029ec8a49afc46
NolzCoding <noel.lauren@outlook.com>
2023-01-19 14:58:19 +0100

    Commet

--- bozogame/Assets/Camera.cs
+++ bozogame/Assets/Camera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     private Transform player;
+    public float speed = 0.09f;
     void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
@@ -16,7 +17,7 @@ public class Camera : MonoBehaviour
         Vector2 target = player.position;
         Vector2 current = transform.position;
 
-        var calculated = Vector2.Lerp(current, target, 0.15f * Time.deltaTime);
+        var calculated = Vector2.Lerp(current, target, speed * Time.deltaTime);
 
         transform.position = new Vector3(calculated.x, calculated.y, -10);
 

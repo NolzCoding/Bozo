e7969c7dd8e236beb78e31119ba641742e0a947f
NolzCoding <noel.lauren@outlook.com>
2023-01-20 13:34:07 +0100

    sdgsdg

--- bozogame/Assets/PickupAble.cs
+++ bozogame/Assets/PickupAble.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,38 @@ using UnityEngine;
 public class PickupAble : MonoBehaviour
 {
     public float healthGiven = 0.01f;
+    private bool shouldSmooth;
+    public SpriteRenderer renderer;
+    private float des = 10;
+    public float time = 0.2f;
+
+    private void Start()
+    {
+        
+        
+    }
+
+    private void Update()
+    {
+        if (shouldSmooth)
+        {
+            var newcol = Mathf.Lerp(renderer.color.b, des, time * Time.deltaTime);
+            renderer.color = new Color(newcol, newcol, newcol);
+            print(newcol);
+            if (newcol < 11)
+            {
+                shouldSmooth = false;
+            }
+        }
+    }
+
 
     public void onPlayerPickup()
     {
+        Debug.Log("edede");
         Destroy(gameObject);
+        shouldSmooth = true;
+        Destroy(gameObject.GetComponent<BoxCollider2D>());
     }
     
 }
